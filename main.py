bba4d2a1ac09bc8161684363ef032aa1ebfb5cfb
bavarescus-netizen <bavarescus@gmail.com>
2026-01-25 21:43:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, Depends
-from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
-from db import get_db
+from sqlalchemy import text
 
-app = FastAPI()
+from app.db import get_db
+from app.routes.prediccion import router as prediccion_router
+from app.routes.historico import router as historico_router
+
+app = FastAPI(title="API Lotto Activo")
+
+app.include_router(prediccion_router)
+app.include_router(historico_router)
 
 
 @app.get("/")
 async def root(db: AsyncSession = Depends(get_db)):
     result = await db.execute(text("select 'API Lotto funcionando 🚀'"))
     return {"estado": result.scalar()}
+
