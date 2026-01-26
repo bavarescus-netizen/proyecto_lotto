818d5e005deffde3a1018054b0d2a0887f47b060
bavarescus-netizen <bavarescus@gmail.com>
2026-01-25 21:14:37 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,25 +1,41 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
+# =============================
+# DATABASE URL (Render / Neon)
+# =============================
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL no configurada en Render")
+    raise ValueError("DATABASE_URL no configurada")
 
-# SQLAlchemy async necesita asyncpg
+# Neon necesita asyncpg
 DATABASE_URL = DATABASE_URL.replace(
     "postgresql://",
     "postgresql+asyncpg://"
 )
 
+# =============================
+# ENGINE
+# =============================
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
     pool_pre_ping=True
 )
 
+# =============================
+# SESSION MAKER
+# =============================
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
-# Dependency para FastAPI
+# =============================
+# DEPENDENCY FASTAPI
+# =============================
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
